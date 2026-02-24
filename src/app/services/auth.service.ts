e9b712f00ef26e8fcd722e7a9597ae412a65c669
Saulix187203 <saulo.ixcoy@gmail.com>
2026-02-23 21:27:24 -0600

    modificando el uso del servicio, siendo  auth.service y login.component, quitando del button las entradas user.value, pass.value

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 
 @Injectable({
-providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-login(username: string, password: string) {
-if (username === 'admin' && password === '1234') {
-return of(true);
-}
-return of(false);
-}
+    login(username: string, password: string) {
+        if (username === 'admin' && password === '1234') {
+            return of({ success: true });
+        }
+        return of({ success: false });
+    }
 }
